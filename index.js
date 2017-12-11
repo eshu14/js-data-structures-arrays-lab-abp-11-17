facbed6ec30eaef6bc3423cab7df7fc4040f9141
Easwari Ramani <eshuprad@gmail.com>
2017-12-11 19:16:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution here!
 const drivers=["Milo", "Otis", "Garfield"];
 function destructivelyAppendDriver(name){
-
+  return drivers.unshift(name);
 }
 function destructivelyPrependDriver(name){
 
